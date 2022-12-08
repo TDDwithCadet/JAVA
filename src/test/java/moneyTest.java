4612fdd859bd416f5245235bcb1dcdd26ed3f22e
sjin <wlstjdeo00@gmail.com>
2022-12-08 09:57:50 +0900

    FEATURE : Franc class를 비교하는 test code 작성 후 Franc class 생성

--- src/test/java/moneyTest.java
+++ src/test/java/moneyTest.java
@@ -12,6 +12,13 @@ class moneyTest {
     }
 
     @Test
+    public void testFrancMultiplcation() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar((6))));
