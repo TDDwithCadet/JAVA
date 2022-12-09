45e91f6f46f38ec1ea77b9633a5cc5bd33f124cc
sjin <wlstjdeo00@gmail.com>
2022-12-09 13:58:33 +0900

    FEATURE : money class 에 dollar, franc class에서 구현한 times를 구현

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-abstract public class Money {
+public class Money {
     protected int amount;
     protected String currency;
 
@@ -9,7 +9,7 @@ abstract public class Money {
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency.equals(money.currency());
     }
 
     static Money dollar(int amount) {
@@ -20,10 +20,16 @@ abstract public class Money {
         return new Franc(amount, "CHF");
     }
 
-    abstract Money times(int multiplier);
-
     String currency() {
         return currency;
+    }
+
+    public String toString() {
+        return amount + " " + currency;
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
     }
 }
 
